29ecf8640893092359142033622fecde0317e329
andrea <andrea@bonanno.cloud>
2020-03-11 11:48:37 +0100

    Initial Commit

--- ebpf2.c
+++ ebpf2.c
@@ -1,50 +1,145 @@
 #include <linux/sched.h>
+#include <linux/nsproxy.h>
+#include <linux/pid_namespace.h>
+
+enum syscall_id{
+SYS_EXECVE,
+SYS_EXIT,
+SYS_UNLINK,
+SYS_OPEN,
+SYS_CLOSE,
+};
 
 typedef struct {
-    u32 pid;
+    u32 real_pid;
+    u32 ns_pid;
+    u32 ns_id;
     u64 ts;
     char comm[TASK_COMM_LEN];
-    char call[TASK_COMM_LEN];
-    //char path[PATH_MAX];
+    enum syscall_id call;
     char path[128];
 } data_t;
 
-//BPF_PERCPU_ARRAY(tmp_arr, data_t, 1);
+BPF_HASH(pids_map, u32, u32);
 BPF_PERF_OUTPUT(events);
+
+static u32 get_task_pid_ns_id(struct task_struct *task)
+{
+    return task->nsproxy->pid_ns_for_children->ns.inum;
+}
+
+static u32 get_task_ns_pid(struct task_struct *task)
+{
+    return task->thread_pid->numbers[task->nsproxy->pid_ns_for_children->level].nr;
+}
+
+static u32 add_pid_ns_if_needed()
+{
+    struct task_struct *task;
+    task = (struct task_struct *)bpf_get_current_task();
+
+    u32 pid_ns = get_task_pid_ns_id(task);
+    if (pids_map.lookup(&pid_ns) != 0)
+        // Container pidns was already added to map
+        return pid_ns;
+
+    // If pid equals 1 - start tracing the container
+    if (get_task_ns_pid(task) == 1) {
+        // A new container/pod was started - add pid namespace to map
+        pids_map.update(&pid_ns, &pid_ns);
+        return pid_ns;
+    }
+
+    // Not a container/pod
+    return 0;
+}
+
+static void remove_pid_ns_if_needed()
+{
+    struct task_struct *task;
+    task = (struct task_struct *)bpf_get_current_task();
+
+    u32 pid_ns = get_task_pid_ns_id(task);
+    if (pids_map.lookup(&pid_ns) != 0) {
+        // If pid equals 1 - stop tracing this pid namespace
+        if (get_task_ns_pid(task) == 1) {
+            pids_map.delete(&pid_ns);
+        }
+    }
+}
+
+static u32 lookup_pid_ns(struct task_struct *task)
+{
+    u32 task_pid_ns = get_task_pid_ns_id(task);
+
+    u32 *pid_ns = pids_map.lookup(&task_pid_ns);
+    if (pid_ns == 0)
+        return 0;
+
+    return *pid_ns;
+}
+
+static int is_container()
+{
+    struct task_struct *task;
+    task = (struct task_struct *)bpf_get_current_task();
+    return lookup_pid_ns(task);
+}
+
+static int init_data(data_t *data){
+    struct task_struct *task;
+    task = (struct task_struct *) bpf_get_current_task();
+    data->ns_pid = get_task_ns_pid(task);
+    data->real_pid = bpf_get_current_pid_tgid();
+    data->ts = bpf_ktime_get_ns();
+    bpf_get_current_comm(&data->comm, sizeof(data->comm));
+    return 0;
+}
 
 int syscall__execve(struct pt_regs *ctx, const char __user *filename) {
 
-    /*
-    u32 key = 0;
-    data_t *datatmp = tmp_arr.lookup(&key);
-    if(datatmp != NULL){
-        datatmp->pid = bpf_get_current_pid_tgid();
-        datatmp->ts = bpf_ktime_get_ns();
-        strcpy(datatmp->call, "execve");
-        bpf_probe_read_str(datatmp->path, sizeof(datatmp->path), filename);
-        bpf_get_current_comm(&datatmp->comm, sizeof(datatmp->comm));
-        }
-    events.perf_submit(ctx, datatmp, sizeof(data_t));
-    */
+    data_t data = {};
+    data.ns_id = add_pid_ns_if_needed();
+    if(!is_container())
+        return 0;
+    init_data(&data);
+    data.call = SYS_EXECVE;
+    bpf_probe_read_str(data.path, sizeof(data.path), filename);
+    events.perf_submit(ctx, &data, sizeof(data));
+    return 0;
+}
+
+int syscall__exit(struct pt_regs *ctx) {
 
     data_t data = {};
-    data.pid = bpf_get_current_pid_tgid();
-    data.ts = bpf_ktime_get_ns();
-    strcpy(data.call, "execve");
-    bpf_probe_read_str(data.path, sizeof(data.path), filename);
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.ns_id = add_pid_ns_if_needed();
+    if(!is_container())
+        return 0;
+    init_data(&data);
+    data.call = SYS_EXIT;
+    remove_pid_ns_if_needed();
     events.perf_submit(ctx, &data, sizeof(data));
     return 0;
 }
 
-int syscall__unlink(struct pt_regs *ctx, const char __user *pathname) {
-  data_t data = {};
-    data.pid = bpf_get_current_pid_tgid();
-    data.ts = bpf_ktime_get_ns();
-    strcpy(data.call, "unlink");
-    bpf_probe_read_str(data.path, sizeof(data.path), pathname);
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    events.perf_submit(ctx, &data, sizeof(data));
+static int trace_generic(u32 id)
+{
+    //int should_trace = is_container();
+    //if(!should_trace)
+      //  return 0;
+    data_t data = {};
+    init_data(&data);
+    data.call = id;
     return 0;
 }
 
+#define TRACE_SYSCALL(name, id)                                         \
+int syscall__##name(struct pt_regs *ctx)                                \
+{                                                                       \
+    trace_generic(id);                                                  \
+    return 0;                                                           \
+}                                                                       \
+
+TRACE_SYSCALL(unlink, SYS_UNLINK);
+TRACE_SYSCALL(open, SYS_OPEN);
+TRACE_SYSCALL(close, SYS_CLOSE);
--- myTest2.py
+++ myTest2.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
 from bcc import BPF
+from enum import Enum, unique
 import yaml
+
+
+@unique
+class Syscalls(Enum):
+    SYS_EXECVE, SYS_EXIT, SYS_UNLINK, SYS_OPEN, SYS_CLOSE = range(5)
+
 
 ebpf_base_filename = 'ebpf2.c'
 filter_filename = 'filter2.yaml'
@@ -13,13 +20,13 @@ with open(filter_filename, 'r') as filterfile:
     filters = yaml.load(filterfile, Loader=yaml.FullLoader)
 
 bpf = BPF(text=ebpf_base_str)
-
+print("These Syscalls will be tracked:\n")
 for call in filters["syscalls"]:
     syscall_fname = bpf.get_syscall_fnname(call)
     print(syscall_fname)
     bpf.attach_kprobe(syscall_fname, fn_name="syscall__" + call)
 
-print("%-18s %-16s %-6s %-10s %s" % ("TIME(s)", "COMM", "PID", "SYSCALL", "PATH"))
+print("\n%-18s %-16s %-16s %-10s %-16s %-10s %s" % ("TIME(s)", "COMM", "REAL_PID", "NS_PID", "NS_ID", "SYSCALL", "PATH"))
 
 start = 0
 
@@ -30,7 +37,7 @@ def print_event(cpu, data, size):
     if start == 0:
         start = event.ts
     time_s = (float(event.ts - start)) / 1000000000
-    print(b"%-18.9f %-16s %-6d %-10s %s" % (time_s, event.comm, event.pid, event.call, event.path))
+    print(b"%-18.9f %-16s %-16d %-10d %-16d %-10s %s" % (time_s, event.comm, event.real_pid, event.ns_pid, event.ns_id, Syscalls(event.call).name, event.path))
 
 
 bpf["events"].open_perf_buffer(print_event)
